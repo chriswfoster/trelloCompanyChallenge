d884a6ffc6bcf82a7c529a353f925756912f4e42
Chris <chriswfoster@gmail.com>
2018-05-26 15:59:31 -0500

    Removed react-beautiful-dnd

--- src/components/AsortedPlayground/AsortingPlayground.js
+++ src/components/AsortedPlayground/AsortingPlayground.js
@@ -1,19 +1,43 @@
 import React, {Component} from 'react'
 
-class AsortingPlayground extends Component{
-constructor(){
-    super()
-    this.state = {
-        first: [],
-        second: []
-    }
-}
-render(){
-return(
-<div>
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend'
+import Container from './Container';
 
-</div>
-)
+class AsortingPlayground extends Component{
+
+    render() {
+		const style = {
+			display: "flex",
+			justifyContent: "space-around",
+			paddingTop: "20px"
+		}
+ 
+		const listOne = [
+			{ id: 1, text: "Item 1" },
+			{ id: 2, text: "Item 2" },
+			{ id: 3, text: "Item 3" }
+		];
+ 
+		const listTwo = [
+			{ id: 4, text: "Item 4" },
+			{ id: 5, text: "Item 5" },
+			{ id: 6, text: "Item 6" }
+		];
+ 
+		const listThree = [
+			{ id: 7, text: "Item 7" },
+			{ id: 8, text: "Item 8" },
+			{ id: 9, text: "Item 9" }
+		];
+ 
+		return (
+			<div style={{...style}}>
+				<Container id={1} list={listOne} />
+				<Container id={2} list={listTwo} />
+				<Container id={3} list={listThree} />
+			</div>
+		);
+	}
 }
-}
-export default AsortingPlayground
+export default DragDropContext(HTML5Backend)(AsortingPlayground);
