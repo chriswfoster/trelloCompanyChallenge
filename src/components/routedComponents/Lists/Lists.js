743455dc66a1dd1d2b989519d6ffc3512cfcb3c2
Chris <chriswfoster@gmail.com>
2018-05-26 13:56:30 -0500

    Changing card type structure to cardlist

--- src/components/routedComponents/Lists/Lists.js
+++ src/components/routedComponents/Lists/Lists.js
@@ -2,15 +2,22 @@ import React, { Component } from "react"
 import { connect } from "react-redux"
 
 import { getUserBoards, boardView } from "../../../ducks/reducer"
-
+import CardList from "./childComponents/CardList"
 
 import "./lists.css"
 
 class Lists extends Component {
   render() {
-    const list = this.props.viewingBoard.lists.map((listitem, i)=> <p>listItem</p>)
+    const listsOfCards = this.props.viewingBoard.lists.map((list, i) => (
+      <CardList listDetails={list} key={i}/>
+    ))
     console.log(this.props)
-    return <div>Something here</div>
+    return (
+    <div>
+      <p>Something here</p> 
+      {listsOfCards}
+      </div>
+      )
   }
 }
 const mapStateToProps = state => state
---
+++ src/components/routedComponents/Lists/childComponents/CardList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class CardList extends Component{
+
+render(){
+    console.log(this.props.listDetails)
+return(
+<div>
+
+</div>
+)
+}
+}
+export default CardList
