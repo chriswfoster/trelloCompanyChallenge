18b0be066bf242886e278521605e804d48048950
Chris <chriswfoster@gmail.com>
2018-05-28 16:56:05 -0500

    backup with comments

--- src/components/routedComponents/Lists/Lists.js
+++ src/components/routedComponents/Lists/Lists.js
@@ -1,31 +1,32 @@
 import React, { Component } from "react"
-import { connect } from "react-redux"
 
-import { getUserBoards, boardView } from "../../../ducks/reducer"
+import { connect } from "react-redux"
+import { DragDropContext } from "react-dnd"
+import HTML5Backend from "react-dnd-html5-backend"
+
 import CardList from "./childComponents/CardList"
-import ListMenu from "./childComponents/ListMenu"
 import Header from "./../../Header/Header"
+import ListMenu from "./childComponents/ListMenu"
 
 import "./lists.css"
 
 class Lists extends Component {
   render() {
-    // I'm not sure how you prefer to map over things and return them. Here I'm mapping in the render. But in CardList, I'm mapping in the return.
-    const listsOfCards = this.props.viewingBoard.lists.map((list, i) => (
-      <CardList listDetails={list} key={i} />
-    ))
-    console.log(this.props)
     return (
       <div className="Lists-primary">
         <Header />
         <ListMenu boardName={this.props.viewingBoard.name} />
-        <div className="Lists-cardFlex">{listsOfCards}</div>
+        <div className="Lists-cardFlex">
+          {this.props.viewingBoard.lists.map((list, i) => (
+            <CardList list={list.cards} key={i} id={i} />
+          ))}
+        </div>
       </div>
     )
   }
 }
 const mapStateToProps = state => state
-export default connect(mapStateToProps, {
-  getUserBoards,
-  boardView
-})(Lists)
+
+export default connect(mapStateToProps, {})(
+  DragDropContext(HTML5Backend)(Lists)
+)
--- src/components/routedComponents/Lists/childComponents/CardList.js
+++ src/components/routedComponents/Lists/childComponents/CardList.js
@@ -1,20 +1,117 @@
 import React, { Component } from "react"
+import update from "immutability-helper"
+import Card from "./Card"
+import { connect } from "react-redux"
+import { sendUpdate } from "../../../../ducks/reducer"
+import { DropTarget } from "react-dnd"
 
-class CardList extends Component {
+class ListMenu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { cards: props.list }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    console.log(nextProps)
+
+    //Please ignore this ternary lol.
+    //For some reason on first card drag, it won't update correctly.
+    return nextProps.list !== this.state.cards ? true : true
+  }
+
+  pushCard(card) {
+    this.setState(
+      update(this.state, {
+        cards: {
+          $push: [card]
+        }
+      })
+    )
+    this.updateReducer()
+  }
+
+  removeCard(index) {
+    this.setState(
+      update(this.state, {
+        cards: {
+          $splice: [[index, 1]]
+        }
+      })
+    )
+    this.updateReducer()
+  }
+
+  moveCard(dragIndex, hoverIndex) {
+    const { cards } = this.state
+    const dragCard = cards[dragIndex]
+
+    this.setState(
+      update(this.state, {
+        cards: {
+          $splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]]
+        }
+      })
+    )
+    console.log("move", this.props.id)
+  }
+
+  updateReducer() {
+    this.props.sendUpdate(
+      this.props.id,
+      this.state.cards,
+      this.props.viewingBoard
+    )
+  }
+
   render() {
-    console.log(this.props.listDetails)
+    const { cards } = this.state
+    const { canDrop, isOver, connectDropTarget } = this.props
+    const isActive = canDrop && isOver
+    const style = {
+      width: "200px",
+      height: "90vh",
+      borderRadius:"3px"
+    }
 
-    return (
-      <div className="CardList-primary">
-        <div className="CardList-secondary">
-          {this.props.listDetails.name}
-          {/* As I said in the Lists component, I'm not sure where you prefer the mapping to go. I'm not sure what best practice is, I've seen it both ways. */}
-          <div className="CardList-cardFlex">
-            {this.props.listDetails.cards.map(card => <p>{card}</p>)}
-          </div>
-        </div>
+    // const backgroundColor = isActive ? "lightgreen" : "lightblue"
+
+    return connectDropTarget(
+      <div className="CardList-primary" style={{ ...style }}>
+        {this.state.cards.map((card, i) => {
+          return (
+            <Card
+              key={card.i}
+              index={i}
+              listId={this.props.id}
+              card={card}
+              cards={this.props.list.cards}
+              removeCard={this.removeCard.bind(this)}
+              moveCard={this.moveCard.bind(this)}
+              updateReducer={this.updateReducer.bind(this)}
+            />
+          )
+        })}
       </div>
     )
   }
 }
-export default CardList
+const cardTarget = {
+  drop(props, monitor, component) {
+    const { id } = props
+    const sourceObj = monitor.getItem()
+    if (id !== sourceObj.listId) component.pushCard(sourceObj.card)
+    return {
+      listId: id
+    }
+  }
+}
+
+const mapStateToProps = state => state
+
+export default connect(mapStateToProps, { sendUpdate })(
+  DropTarget("CARD", cardTarget, (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  }))(ListMenu)
+)
