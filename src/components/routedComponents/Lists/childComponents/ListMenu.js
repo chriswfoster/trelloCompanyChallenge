1a7f321143f366d6fa06fbf27788931de4baf1e8
Chris <chriswfoster@gmail.com>
2018-06-27 11:46:54 -0500

    Design for rename board setup

--- src/components/routedComponents/Lists/childComponents/ListMenu.js
+++ src/components/routedComponents/Lists/childComponents/ListMenu.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react"
+import { Modal, Button, Input } from "antd"
+import "antd/dist/antd.css"
 
 class ListMenu extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showModal: false,
+      renameText: ""
+    }
+  }
+
+  handleCancel = e => {
+    this.setState({ showModal: false, renameText: "" })
+  }
+
+  handleOkay = e => {
+    console.log(e)
+    this.setState({ showMOdal: false })
+  }
+
   render() {
-    return <div className="ListMenu-primary">{this.props.boardName}</div>
+    return (
+      <div>
+        <Button
+          className="ListMenu-primary"
+          onClick={() => this.setState({ showModal: true })}
+        >
+          {this.props.boardName}
+        </Button>
+
+        <Modal
+          title="Basic Modal"
+          visible={this.state.showModal}
+          onOk={this.handleOkay}
+          onCancel={this.handleCancel}
+        >
+          <p>Rename this board?</p>
+          <Input onChange={e => this.setState({renameText: e.target.value})} />
+        </Modal>
+      </div>
+    )
   }
 }
 export default ListMenu
