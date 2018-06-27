6cb32316a3ee42becd441b7b083ffccd3aef0fc2
Chris <chriswfoster@gmail.com>
2018-06-27 12:46:29 -0500

    Dropdown for boards added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,45 @@
-import React, {Component} from 'react'
+import React, { Component } from "react"
+import { Button, Menu, Icon, Dropdown, Input } from "antd"
+import { connect } from "react-redux"
 
-import './header.css'
-class Header extends Component{
+import "./header.css"
+class Header extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showModal: false
+    }
+  }
 
-render(){
-return(
-<div className="Header-primary">
-<p>Rtello</p>
-</div>
-)
+  showModal() {
+    this.setState({ showModal: false })
+  }
+
+  render() {
+    const menu = (  <Menu
+      // visible={this.state.showModal
+    >
+      <Menu.Item>1st menu item</Menu.Item>
+      <Menu.Item><Input /></Menu.Item>
+    </Menu>)
+    return (
+      <div className="Header-primary">
+        <Button
+          onClick={() => this.setState({ showModal: true })}
+          type="primary"
+        >
+          <Dropdown overlay={menu}>
+    <p className="ant-dropdown-link" >
+      Boards <Icon type="down" />
+    </p>
+  </Dropdown>
+        </Button>
+     
+        <p>Rtello</p>
+        <p>Console stuff</p>
+      </div>
+    )
+  }
 }
-}
-export default Header
+const mapStateToProps = state => state
+export default connect(mapStateToProps, {})(Header)
