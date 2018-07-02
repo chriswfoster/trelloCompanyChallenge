089efdcb7b4501c001211c036973ecdab279d8b3
Chris <chriswfoster@gmail.com>
2018-07-02 17:43:39 -0500

    Fixed some warnings

--- src/components/routedComponents/About/About.js
+++ src/components/routedComponents/About/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react"
 import Header from "../../Header/Header"
 import "./about.css"
-import Why from './Why'
-import What from './What'
+import Why from "./Why"
+import What from "./What"
 
 import { Link } from "react-router-dom"
 import { Menu, Icon } from "antd"
@@ -53,8 +53,12 @@ class About extends Component {
               </span>
             }
           >
-            <Menu.Item key="1" onClick={()=> this.generalInfo(<What />)}>What does this site do?</Menu.Item>
-            <Menu.Item key="2" onClick={()=> this.generalInfo(<Why />)}>Why build this?</Menu.Item>
+            <Menu.Item key="1" onClick={() => this.generalInfo(<What />)}>
+              What does this site do?
+            </Menu.Item>
+            <Menu.Item key="2" onClick={() => this.generalInfo(<Why />)}>
+              Why build this?
+            </Menu.Item>
           </SubMenu>
           <SubMenu
             key="sub2"
@@ -82,17 +86,26 @@ class About extends Component {
             }
           >
             <Menu.Item key="9">
-              <a target="_blank" href="https://www.javascript.com/">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.javascript.com/"
+              >
                 Javascript (primary language)
               </a>
             </Menu.Item>
             <Menu.Item key="10">
-              <a target="_blank" href="https://reactjs.org//">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://reactjs.org//"
+              >
                 ReactJS
               </a>
             </Menu.Item>
             <Menu.Item key="11">
               <a
+                rel="noopener noreferrer"
                 target="_blank"
                 href="https://github.com/ReactTraining/react-router"
               >
@@ -100,17 +113,26 @@ class About extends Component {
               </a>
             </Menu.Item>
             <Menu.Item key="12">
-              <a target="_blank" href="https://github.com/reduxjs/react-redux">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/reduxjs/react-redux"
+              >
                 React-Redux
               </a>
             </Menu.Item>
             <Menu.Item key="13">
-              <a target="_blank" href="https://expressjs.com/">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://expressjs.com/"
+              >
                 Node/Express
               </a>
             </Menu.Item>
             <Menu.Item key="14">
               <a
+                rel="noopener noreferrer"
                 target="_blank"
                 href="https://en.wikipedia.org/wiki/Representational_state_transfer"
               >
@@ -118,27 +140,47 @@ class About extends Component {
               </a>
             </Menu.Item>
             <Menu.Item key="15">
-              <a target="_blank" href="https://www.postgresql.org/">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.postgresql.org/"
+              >
                 PostgreSQL
               </a>
             </Menu.Item>
             <Menu.Item key="16">
-              <a target="_blank" href="https://github.com/dmfay/massive-js">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/dmfay/massive-js"
+              >
                 MassiveJS
               </a>
             </Menu.Item>
             <Menu.Item key="17">
-              <a target="_blank" href="https://firebase.google.com/docs/auth/">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://firebase.google.com/docs/auth/"
+              >
                 Firebase Authentication
               </a>
             </Menu.Item>
             <Menu.Item key="18">
-              <a target="_blank" href="https://github.com/react-dnd/react-dnd">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/react-dnd/react-dnd"
+              >
                 React DND (Drag and Drop)
               </a>
             </Menu.Item>
             <Menu.Item key="19">
-              <a target="_blank" href="https://ant.design/">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://ant.design/"
+              >
                 Ant Design
               </a>
             </Menu.Item>
